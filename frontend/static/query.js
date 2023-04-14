6e8be4c2512ba39662dac3cb6af239f5a8b44437
Kyle Cheung <kylecheung22@gmail.com>
2023-04-14 14:24:19 -0400

    updated query.js for optimizations

--- frontend/static/query.js
+++ frontend/static/query.js
@@ -1,16 +1,14 @@
-// Listen for query send button
+const queryHistory = document.getElementById("queryHistory");
 const sendButton = document.getElementById("sendButton");
+
 sendButton.addEventListener("click", async () => {
     const userQueryInput = document.getElementById("userQuery");
     const userQuery = userQueryInput.value;
     
-    //Determine which model to use
     const dropdown = document.getElementById("apiDropdown");
     const selectedModel = dropdown.value;
 
-    //Clear the input
     userQueryInput.value = "";
-    //Add user input to query history
     updateQueryHistory(userQuery);
 
     const response = await fetch("/store_user_query", {
@@ -23,18 +21,16 @@ sendButton.addEventListener("click", async () => {
     
     const result = await response.json();
     if (result.status === "success") {
-        //Add response from app to query history
         const sqlResults = JSON.parse(result.message.sql_results);
-        updateQueryHistory(`${result.message.sql_query}`, true, sqlResults);
+        const sqlQuery = result.message.sql_query;
+        updateQueryHistory(sqlQuery, true, sqlResults);
     } else {
-        // Handle the error here (e.g., show an error message)
-        updateQueryHistory("There was an error with your query", true)
+        updateQueryHistory("There was an error with your query", true);
     }
-    // Scroll to the bottom of the queryHistory container
+
     scrollToBottom(queryHistory);
 });
 
-// Capture the "Enter" key press in the userQuery input
 document.getElementById("userQuery").addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         event.preventDefault();
@@ -42,80 +38,83 @@ document.getElementById("userQuery").addEventListener("keydown", (event) => {
     }
 });
 
+async function createFormattedSqlElement(query) {
+    const pre = document.createElement("pre");
+    const code = document.createElement("code");
+
+    const SqlFormatterModule = await import("https://cdn.skypack.dev/sql-formatter@2.3.3");
+    const formattedQuery = SqlFormatterModule.default.format(query);
+
+    code.classList.add("language-sql");
+    code.textContent = formattedQuery;
+    pre.appendChild(code);
+
+    Prism.highlightElement(code);
+
+    return pre;
+}
+
+function createResultsTable(results) {
+    const tableId = `resultsTable_${Date.now()}`;
+
+    const resultsTable = document.createElement("table");
+    resultsTable.id = tableId;
+    resultsTable.classList.add("display");
+
+    const thead = document.createElement("thead");
+    const headerRow = document.createElement("tr");
+    Object.keys(results[0]).forEach((key) => {
+        const th = document.createElement("th");
+        th.textContent = key;
+        headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    resultsTable.appendChild(thead);
+
+    const tbody = document.createElement("tbody");
+    results.forEach((row) => {
+        const tr = document.createElement("tr");
+        Object.values(row).forEach((value) => {
+            const td = document.createElement("td");
+            td.textContent = value;
+            tr.appendChild(td);
+        });
+        tbody.appendChild(tr);
+    });
+    resultsTable.appendChild(tbody);
+
+    return resultsTable;
+}
 
 async function updateQueryHistory(query, sequel_response = false, results = null) {
-    const queryHistory = document.getElementById("queryHistory");
     const queryElement = document.createElement("p");
     const separator = document.createElement("hr");
 
     if (sequel_response) {
         queryElement.classList.add("sequelResponse");
-        const separator = document.createElement("hr");
         queryHistory.appendChild(separator);
 
         if (query) {
             queryElement.classList.add("preserve-whitespace");
-            // Wrap the SQL query in a <pre> and <code> element
-            const pre = document.createElement("pre");
-            const code = document.createElement("code");
-
-            // Use dynamic import() to load the SqlFormatter library
-            const SqlFormatterModule = await import("https://cdn.skypack.dev/sql-formatter@2.3.3");
-            const formattedQuery = SqlFormatterModule.default.format(query);
-
-            code.classList.add("language-sql"); // Add the Prism.js SQL language class
-            code.textContent = formattedQuery;
-            pre.appendChild(code);
-            queryElement.appendChild(pre);
-
-            // Add Prism.js syntax highlighting
-            Prism.highlightElement(code);
+            queryElement.appendChild(await createFormattedSqlElement(query));
         }
     } else {
         queryElement.textContent = query;
     }
 
     queryHistory.appendChild(queryElement);
-    // Scroll to the bottom of the queryHistory container
-    scrollToBottom(queryHistory);
 
     if (results) {
-        // Create a unique ID for each table
-        const tableId = `resultsTable_${Date.now()}`;
-        
-        const resultsTable = document.createElement("table");
-        resultsTable.id = tableId; // Set the ID for the table
-        resultsTable.classList.add("display"); // Add the DataTables class
-
-        const thead = document.createElement("thead");
-        const headerRow = document.createElement("tr");
-        Object.keys(results[0]).forEach((key) => {
-            const th = document.createElement("th");
-            th.textContent = key;
-            headerRow.appendChild(th);
-        });
-        thead.appendChild(headerRow);
-        
-        resultsTable.appendChild(thead);
-
-        const tbody = document.createElement("tbody");
-        results.forEach((row) => {
-            const tr = document.createElement("tr");
-            Object.values(row).forEach((value) => {
-                const td = document.createElement("td");
-                td.textContent = value;
-                tr.appendChild(td);
-            });
-            tbody.appendChild(tr);
-        });
-
-        resultsTable.appendChild(tbody);
+        const resultsTable = createResultsTable(results);
         queryHistory.appendChild(resultsTable);
         queryHistory.appendChild(separator);
-        
+
         // Initialize DataTables for the created table
-        $(`#${tableId}`).DataTable();
+        $(`#${resultsTable.id}`).DataTable();
     }
+
+    // Scroll to the bottom of the queryHistory container
+    scrollToBottom(queryHistory);
 }
 
 function scrollToBottom(element) {
